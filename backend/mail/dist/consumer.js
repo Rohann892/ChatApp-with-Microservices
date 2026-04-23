462558e55def425accfb2d6c5ac1c1eaa8715d64
Rohan Lal <lalrohan857@gmail.com>
2026-04-23 18:48:30 +0530

    first commit

---
+++ backend/mail/dist/consumer.js
@@ -0,0 +1,50 @@
+import amqp from "amqplib";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+export const startSendOtpConsumer = async () => {
+    try {
+        const connection = await amqp.connect({
+            protocol: "amqp",
+            hostname: process.env.RABBITMQ_HOST,
+            port: 5672,
+            username: process.env.RABBITMQ_USERNAME,
+            password: process.env.RABBITMQ_PASSWORD,
+        });
+        const channel = await connection.createChannel();
+        const queueName = "send-otp";
+        await channel.assertQueue(queueName, { durable: true });
+        console.log("✅ mail service started, listening for otp emails");
+        channel.consume(queueName, async (msg) => {
+            if (msg) {
+                try {
+                    const { to, subject, body } = JSON.parse(msg.content.toString());
+                    const trasnporter = nodemailer.createTransport({
+                        host: "smtp.gmail.com",
+                        port: 465,
+                        auth: {
+                            user: process.env.USER,
+                            pass: process.env.PASSWORD,
+                        },
+                    });
+                    await trasnporter.sendMail({
+                        from: "chat app",
+                        to,
+                        subject,
+                        text: body,
+                    });
+                    console.log("✅ otp email sent successfully");
+                    channel.ack(msg);
+                }
+                catch (error) {
+                    console.log("Failed to send otp error", error);
+                    channel.nack(msg);
+                }
+            }
+        });
+    }
+    catch (error) {
+        console.log("Send otp error", error);
+    }
+};
+//# sourceMappingURL=consumer.js.map
---
+++ backend/user/dist/config/rabbitmq.js
@@ -0,0 +1,31 @@
+import amqp, {} from "amqplib";
+let channel;
+let connection;
+export const connectRabbitMQ = async () => {
+    try {
+        connection = await amqp.connect({
+            protocol: "amqp", // ✅ fixed typo (was "ampq")
+            hostname: process.env.RABBITMQ_HOST,
+            port: 5672,
+            username: process.env.RABBITMQ_USERNAME,
+            password: process.env.RABBITMQ_PASSWORD,
+        });
+        channel = await connection.createChannel();
+        console.log("✅ Connected to RabbitMQ");
+    }
+    catch (error) {
+        console.error("❌ Failed to connect to RabbitMQ", error);
+    }
+};
+export const publishToQueue = async (queueName, message) => {
+    if (!channel) {
+        console.log("RabbitMQ is not inatalized");
+        return;
+    }
+    await channel.assertQueue(queueName, { durable: true });
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+        persistent: true,
+    });
+    console.log(`Message sent to queue: ${queueName}`);
+};
+//# sourceMappingURL=rabbitmq.js.map
